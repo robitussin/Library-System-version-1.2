8f273a1e85106bbf451f2d2e28ef62774462dbcb
robitussin <elizerponiojr@gmail.com>
2020-01-20 12:05:47 +0800

    Updated file path in bat file

--- Eric's project v1.2/out/artifacts/Eric_s_project_jar/LibrarySystem.bat
+++ Eric's project v1.2/out/artifacts/Eric_s_project_jar/LibrarySystem.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-java --module-path C:\Users\Robbie\Desktop\javafx-sdk-13.0.1\lib --add-modules javafx.controls,javafx.fxml -jar LibrarySystem.jar
+java --module-path C:\javafx-sdk-13.0.1\lib --add-modules javafx.controls,javafx.fxml -jar LibrarySystem.jar
